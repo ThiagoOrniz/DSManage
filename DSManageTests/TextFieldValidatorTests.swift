cd7855c889886a02dfbf8eda14563d94b5042d35
Thiago Orniz Martin <thiago.orniz@gmail.com>
2017-01-09 00:41:48 -0200

    start adding ui and unit tests

---
+++ DSManageTests/TextFieldValidatorTests.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewControllerTests.swift
+//  DSManage
+//
+//  Created by Thiago Orniz Martin on 09/01/17.
+//  Copyright © 2017 Thiago Orniz Martin. All rights reserved.
+//
+
+import XCTest
+@testable import DSManage
+
+class TextFieldValidatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testValidEmail() {
+        
+        var email = "email@test.com"
+        XCTAssert(TextfieldValidator.validateEmail(email))
+        
+        email = "em"
+        XCTAssertFalse(TextfieldValidator.validateEmail(email))
+
+    }
+    
+    func testValidPassword() {
+        
+        var password = "password"
+        XCTAssert(TextfieldValidator.validatePassword(password))
+        
+        password = "s"
+        XCTAssertFalse(TextfieldValidator.validatePassword(password))
+    }
+    
+    func testValidName() {
+        
+        var name = "nameOk"
+        XCTAssert(TextfieldValidator.validateName(name))
+        
+        name = ""
+        XCTAssertFalse(TextfieldValidator.validateName(name))
+    }
+}
--- DSManageUITests/DSManageUITests.swift
+++ DSManageUITests/DSManageUITests.swift
@@ -13,25 +13,36 @@ class DSManageUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
+    func testValidateEmailAndPassword_thenLogin() {
+    
+        let app = XCUIApplication()
+        let scrollViewsQuery = app.scrollViews
         
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let elementsQuery = scrollViewsQuery.otherElements
+        elementsQuery.textFields["email"].tap()
+        elementsQuery.textFields["email"].typeText("oi@oi.com")
+        
+        elementsQuery.textFields["password"].tap()
+        elementsQuery.textFields["password"].typeText("123456")
+        
+       let login = XCUIApplication().scrollViews.otherElements.buttons["Log in"]
+        login.tap()
+        
+        let alertEmail = app.alerts["Invalid email"]
+        let alertPassword = app.alerts["Invalid password"]
+        
+        XCTAssertFalse(alertEmail.exists)
+        XCTAssertFalse(alertPassword.exists)
+        
+        
     }
     
 }
